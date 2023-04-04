4748fda7bb9786f172348b7abda55cf9c03f4963
sumeet-at-scaler <sumeet@scaler.com>
2023-04-04 20:03:03 +0530

    Code

---
+++ CodeForEditorial/src/main/java/Client.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            TableCreator tc = new TableCreator(i);
+            Thread t = new Thread(tc);
+            t.start();
+        }
+
+    }
+}
